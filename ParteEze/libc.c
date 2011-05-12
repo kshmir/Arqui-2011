a9b96f6587eeee8a10361efacf8e7543ff0c0f4f
Eze-VM <moroco.ortiz@gmail.com>
2011-05-12 13:04:05 -0300

    separado en librerias shell simulacion

---
+++ ParteEze/libc.c
@@ -0,0 +1,232 @@
+#include "defs.h"
+#include <stdarg.h>
+#include <stdio.h>
+#include "libc.h"
+
+void myftoa(float number, char* answ){
+
+	char ascii0 = ASCIICERO;
+	int pos = 0;
+	int decimal = 6;
+	
+	char sign = FALSE;
+	if (number<0){
+		sign = number<0?TRUE:FALSE;
+		number*=-1;
+	}
+	answ[pos++]='\0';
+	
+	if ((int)number==0){
+				answ[pos++]='0';
+	}
+	/*
+	 * This is used to get a integer number.
+	 * 
+	 */ 
+	number*=1000000;
+	number=(int)number; 
+	while(decimal){
+		answ[pos++]=(int)number%10+ASCIICERO;
+		number/=10;
+		decimal--;
+	}
+	answ[pos++]='.';
+	while((int)number!=0){
+		answ[pos++]=(int)number%10+ASCIICERO;
+		number/=10;
+		
+	}	
+	
+	/*
+	 * This is because when it get out of the whie it has been divide per 10, one more time 
+	 */
+	number*=10;
+	if(sign == TRUE)
+		answ[pos++]='-';
+	
+	internalswap(answ,pos-1);
+	
+	
+}
+
+void internalswap(char* answ,int pos){
+
+	
+	int correccion=0;
+	int i = 0;
+	correccion += pos%2;
+	while(i<(pos+correccion)/2){
+		char aux = answ[i];
+		answ[i]=answ[pos-i];
+		answ[pos-i]=aux;
+		i++;
+	} 
+}
+
+void myitoa(int number, char* answ){
+
+	int pos = 0;
+	
+	char sign = FALSE;
+	if (number<0){
+		sign = number<0?TRUE:FALSE;
+		number*=-1;
+	}
+		
+	answ[pos++]='\0';
+	if (number ==0)
+		answ[pos++]=ASCIICERO;
+	else{	
+		while(number!=0){
+			answ[pos++]=(number%10)+ASCIICERO;
+			number/=10;
+		}
+		if(sign == TRUE)
+			answ[pos++]='-';
+	
+		internalswap(answ,pos-1);
+	}
+}
+
+void printstring(char* message){
+	int i =0;
+	while(message[i]!='\0'){
+		putchar(message[i]);
+		i++;
+	}
+}
+	
+void printdouble(double number, char* format){
+	
+
+		char chardouble[40];
+		
+		myftoa(number, chardouble);
+		int i=0;
+		while (chardouble[i]!='\0'){
+				putchar(chardouble[i]);
+				i++;
+		}
+}
+
+int myatoi(char* string){
+	
+	int i=0;
+	int result=0;
+	int sign =1;
+	
+	while(string[i]!='\0'){
+		if(string[i]=='-'){
+			sign=-1;
+			i++;
+		}
+		result=result*10 + (string[i]-ASCIICERO);
+		i++;
+	}
+	return result*sign;
+		
+}
+
+double myatof(char*string){
+	int sign = 1;
+	int decount=0;
+	int i=0;
+	double result= 0.0;
+	double decimals = 0.1;
+
+	while(string[i]!='\0'){
+		if (string[i]=='.'){
+			decount=1;
+			i++;
+		}
+		else{
+			if(!decount){
+				if(string[i]=='-'){
+					i++;
+					sign=-1;
+				}
+				else{
+					result=result*10 + (string[i]-ASCIICERO);
+					i++;
+				}
+			}
+			else {
+				result=result + decimals*(string[i]-ASCIICERO);
+				decimals/=10;
+				i++;
+			}
+		}
+	}
+	return result*sign;
+}
+void myprintf(char* string, ...){
+	
+	int i = 0, va_count;
+	va_list ap;
+	
+	va_start(ap, string);
+	
+	while(string[i]!='\0'){
+		if (string[i]=='%'){
+			i++;
+			switch(string[i]){
+				case 'd':printint(va_arg(ap,int),string);
+				break;
+				case 's':printstring(va_arg(ap,char*));
+				break;
+				case 'c':putchar(va_arg(ap,int));
+				break;
+				case 'f':printdouble(va_arg(ap,double),string);
+				break;
+				case '%':putchar('%');
+				break;
+				
+				default: printstring("\n invalid argument type error \n");
+				
+			}
+		i++;
+		}
+		else {
+			putchar(string[i]);
+			i++;
+		}
+	}
+	va_end(ap);
+
+}
+
+void printint(int number, char* format){
+		
+		char charint[20];
+	
+		myitoa(number, charint);
+				
+		int i=0;
+		while (charint[i]!='\0')
+				putchar(charint[i++]);
+		
+		
+}
+
+int getint(const char mensaje[], ... ){
+	int n, salir = 0;
+	va_list ap;
+		
+	do	
+	{
+		va_start(ap, mensaje);
+		vprintf(mensaje, ap);
+		va_end(ap);
+
+		if ( scanf("%d",&n) != 1)
+		{
+			myprintf("\nInvalid Value, please Try agian\n");
+			BORRA_BUFFER;
+		}
+		else
+			salir = 1;
+	} while (! salir);
+	BORRA_BUFFER;
+	return n;
+}
+
--- ParteEze/simula.c
+++ ParteEze/simula.c
@@ -1,41 +1,8 @@
+#include "libc.h"
+#include "defs.h"
 #include <stdio.h>
-#include <stdarg.h>
-
-#define ASCIICERO 0x30
-#define MARIO 0
-#define LUIGI 1
-#define MARIOST "mario"
-#define LUIGIST "luigi"
-#define OSDEFST "@murcielago:~#"
-#define HELP 0
-#define CLEAR 1
-#define ECHO 2
-#define CPUSPEED 3
-#define SU 4
-#define LOGOUT 5
-#define REBOOT 6
-#define TRUE 1
-#define FALSE 0
-
-#define BORRA_BUFFER while (getchar() != '\n')
-
-double myatof(char*string);
-
-int myatoi(char* string);
-
-void printdouble(double number, char* format);
-
-void internalswap(char* answ,int pos);
 
 void printHelp();
-
-void printstring(char* message);
-
-void myitoa(int number, char* resp);
-
-void printint(int number, char* format);
-
-void myprintf(char* string, ...);
 
 void selectcmd(int value, char** usr);
 
@@ -45,17 +12,12 @@ void init();
 
 int getcommand();
 
-int getint(const char mensaje[], ... );
-
 char* getusr();
-
-void myftoa(float number, char* answ);
-
 
 int main(){
 
 	char* usr;
-	int cmd;
+	int cmd, opt;
 	printdouble(1.23456, usr);
 	printstring("\n");
 	printstring("hola%\n");
@@ -64,6 +26,12 @@ int main(){
 	myprintf("%s -> %d\n","-1567", myatoi("-1567"));
 	myprintf("%s -> %f\n","1.567", myatof("1.567"));
 	myprintf("%s -> %f\n","-1.567", myatof("-1.567"));
+	scanf("%d ",&cmd);
+	BORRA_BUFFER;
+	myprintf(" numero %d %d\n",cmd,opt);
+	scanf("a%da%da",&cmd,&opt);
+	myprintf(" numero %d %d\n",cmd,opt);
+	//si no se sigue el formato pedido devuelve lo que hay en los punteros
 	init();
 	usr = getusr();
 	do{
@@ -92,28 +60,6 @@ void init(){
 
 }
 
-int getint(const char mensaje[], ... ){
-	int n, salir = 0;
-	va_list ap;
-		
-	do	
-	{
-		va_start(ap, mensaje);
-		vprintf(mensaje, ap);
-		va_end(ap);
-
-		if ( scanf("%d",&n) != 1)
-		{
-			myprintf("\nInvalid Value, please Try agian\n");
-			BORRA_BUFFER;
-		}
-		else
-			salir = 1;
-	} while (! salir);
-	BORRA_BUFFER;
-	return n;
-}
-
 char* getusr(){
 	int usrid;
 	int flag = 0;
@@ -127,6 +73,13 @@ char* getusr(){
 	else if (usrid == LUIGI){
 		flag=1;
 		return LUIGIST;
+	}
+	else{
+		/*
+		 * this is made to turn off all warnings
+		 */
+		char* end = MARIOST;
+		return end;
 	}
 	}while(!flag);
 	
@@ -173,208 +126,3 @@ char* su(char* usr){
 	return (usr == MARIOST?LUIGIST:MARIOST);
 }
 
-void myprintf(char* string, ...){
-	
-	int i = 0, va_count;
-	va_list ap;
-	
-	va_start(ap, string);
-	
-	while(string[i]!='\0'){
-		if (string[i]=='%'){
-			i++;
-			switch(string[i]){
-				case 'd':printint(va_arg(ap,int),string);
-				break;
-				case 's':printstring(va_arg(ap,char*));
-				break;
-				case 'c':putchar(va_arg(ap,int));
-				break;
-				case 'f':printdouble(va_arg(ap,double),string);
-				break;
-				case '%':putchar('%');
-				break;
-				
-				default: printstring("\n invalid argument type error \n");
-				
-			}
-		i++;
-		}
-		else {
-			putchar(string[i]);
-			i++;
-		}
-	}
-	va_end(ap);
-
-}
-
-void printint(int number, char* format){
-		
-		char charint[20];
-	
-		myitoa(number, charint);
-				
-		int i=0;
-		while (charint[i]!='\0')
-				putchar(charint[i++]);
-		
-		
-}
-
-void myftoa(float number, char* answ){
-
-	char ascii0 = ASCIICERO;
-	int pos = 0;
-	int decimal = 6;
-	
-	char sign = FALSE;
-	if (number<0){
-		sign = number<0?TRUE:FALSE;
-		number*=-1;
-	}
-	answ[pos++]='\0';
-	
-	if ((int)number==0){
-				answ[pos++]='0';
-	}
-	/*
-	 * This is used to get a integer number.
-	 * 
-	 */ 
-	number*=1000000;
-	number=(int)number; 
-	while(decimal){
-		answ[pos++]=(int)number%10+ASCIICERO;
-		number/=10;
-		decimal--;
-	}
-	answ[pos++]='.';
-	while((int)number!=0){
-		answ[pos++]=(int)number%10+ASCIICERO;
-		number/=10;
-		
-	}	
-	
-	/*
-	 * This is because when it get out of the whie it has been divide per 10, one more time 
-	 */
-	number*=10;
-	if(sign == TRUE)
-		answ[pos++]='-';
-	
-	internalswap(answ,pos-1);
-	
-	
-}
-
-void internalswap(char* answ,int pos){
-
-	
-	int correccion=0;
-	int i = 0;
-	correccion += pos%2;
-	while(i<(pos+correccion)/2){
-		char aux = answ[i];
-		answ[i]=answ[pos-i];
-		answ[pos-i]=aux;
-		i++;
-	} 
-}
-
-void myitoa(int number, char* answ){
-
-	int pos = 0;
-	
-	char sign = FALSE;
-	if (number<0){
-		sign = number<0?TRUE:FALSE;
-		number*=-1;
-	}
-		
-	answ[pos++]='\0';
-	if (number ==0)
-		answ[pos++]=ASCIICERO;
-	else{	
-		while(number!=0){
-			answ[pos++]=(number%10)+ASCIICERO;
-			number/=10;
-		}
-		if(sign == TRUE)
-			answ[pos++]='-';
-	
-		internalswap(answ,pos-1);
-	}
-}
-
-void printstring(char* message){
-	int i =0;
-	while(message[i]!='\0'){
-		putchar(message[i]);
-		i++;
-	}
-}
-	
-void printdouble(double number, char* format){
-	
-
-		char chardouble[40];
-		
-		myftoa(number, chardouble);
-		int i=0;
-		while (chardouble[i]!='\0'){
-				putchar(chardouble[i]);
-				i++;
-		}
-}
-
-int myatoi(char* string){
-	
-	int i=0;
-	int result=0;
-	int sign =1;
-	
-	while(string[i]!='\0'){
-		if(string[i]=='-'){
-			sign=-1;
-			i++;
-		}
-		result=result*10 + (string[i]-ASCIICERO);
-		i++;
-	}
-	return result*sign;
-		
-}
-
-double myatof(char*string){
-	int sign = 1;
-	int decount=0;
-	int i=0;
-	double result= 0.0;
-	double decimals = 0.1;
-
-	while(string[i]!='\0'){
-		if (string[i]=='.'){
-			decount=1;
-			i++;
-		}
-		else{
-			if(!decount){
-				if(string[i]=='-'){
-					i++;
-					sign=-1;
-				}
-				else{
-					result=result*10 + (string[i]-ASCIICERO);
-					i++;
-				}
-			}
-			else {
-				result=result + decimals*(string[i]-ASCIICERO);
-				decimals/=10;
-				i++;
-			}
-		}
-	}
-	return result*sign;
-}
