2c891ba72a5821d0f2b9fda5246ca2a892ee5b44
Eze-VM <moroco.ortiz@gmail.com>
2011-05-11 23:18:59 -0300

    printf done, con argumentos variables se llama myprintf

--- ParteEze/simula.c
+++ ParteEze/simula.c
@@ -25,7 +25,7 @@ void internalswap(char* answ,int pos);
 
 void printHelp();
 
-void printstirng(char* message);
+void printstring(char* message);
 
 void myitoa(int number, char* resp);
 
@@ -53,13 +53,14 @@ int main(){
 	char* usr;
 	int cmd;
 	printdouble(1.23456, usr);
-	printstirng("\n");
-	printstirng("hola\n");
+	printstring("\n");
+	printstring("hola%\n");
+	myprintf("hola %f charola %d soy %c y vos sos %s \n", 123.7, 25, 'h',"sofia");
 	init();
 	usr = getusr();
 	do{
 		do{
-			printf("%s%s ",usr,OSDEFST);
+			myprintf("%s%s ",usr,OSDEFST);
 			cmd=getcommand();
 		}while(cmd<HELP || cmd>LOGOUT);
 		selectcmd(cmd, &usr);
@@ -70,7 +71,7 @@ int main(){
 void init(){
 
 	int i = 0;
-	printf("Muricelag O.S. is loading");
+	myprintf("Muricelag O.S. is loading");
 	
 	for(;i<20;i++){
 		int j=0;
@@ -79,7 +80,7 @@ void init(){
 		}
 		putchar('.');
 	}
-	printf("100 %% \n");
+	myprintf("100 %% \n");
 
 }
 
@@ -95,7 +96,7 @@ int getint(const char mensaje[], ... ){
 
 		if ( scanf("%d",&n) != 1)
 		{
-			printf("\nInvalid Value, please Try agian\n");
+			myprintf("\nInvalid Value, please Try agian\n");
 			BORRA_BUFFER;
 		}
 		else
@@ -109,7 +110,7 @@ char* getusr(){
 	int usrid;
 	int flag = 0;
 	do{
-	printf("Please Select User:\n");
+	myprintf("Please Select User:\n");
 	usrid= getint("0-> Mario (root) \n1-> Luigi\nOption:");
 	if (usrid ==MARIO){
 		flag=1;
@@ -130,16 +131,16 @@ int getcommand(){
 
 void printHelp(){
 	
-	printf("Murcielago bash, version 1.0.0(1)-release (i686-pc-murcielago)\n");
-	printf("These shell commands are defined internally.  Type `help' to see this list.\n");
-	printf("Type `help name' to find out more about the function `name'.\n");
-	printf("Use `info bash' to find out more about the shell in general.\n\n");
-	printf("%d->help [pattern ...]\n", HELP);
-	printf("%d->clear\n", CLEAR);
-	printf("%d->echo [arg ...]\n", ECHO);
-	printf("%d->cpuspeed\n", CPUSPEED);
-	printf("%d->su\n", SU);
-	printf("%d->logout [n]\n", LOGOUT);	
+	myprintf("Murcielago bash, version 1.0.0(1)-release (i686-pc-murcielago)\n");
+	myprintf("These shell commands are defined internally.  Type `help' to see this list.\n");
+	myprintf("Type `help name' to find out more about the function `name'.\n");
+	myprintf("Use `info bash' to find out more about the shell in general.\n\n");
+	myprintf("%d->help [pattern ...]\n", HELP);
+	myprintf("%d->clear\n", CLEAR);
+	myprintf("%d->echo [arg ...]\n", ECHO);
+	myprintf("%d->cpuspeed\n", CPUSPEED);
+	myprintf("%d->su\n", SU);
+	myprintf("%d->logout [n]\n", LOGOUT);	
 	
 }
 
@@ -149,11 +150,11 @@ void selectcmd(int opt, char** usr){
 		break;
 		case SU: *usr=su(*usr);
 		break;
-		case CLEAR : printf("cablear clear screen maxi\n");
+		case CLEAR : myprintf("cablear clear screen maxi\n");
 		break;
-		case ECHO : printf("crear echo\n");
+		case ECHO : myprintf("crear echo\n");
 		break;
-		case CPUSPEED : printf("es muy alta \n");
+		case CPUSPEED : myprintf("es muy alta \n");
 		break;
 		
 	}
@@ -164,28 +165,48 @@ char* su(char* usr){
 	return (usr == MARIOST?LUIGIST:MARIOST);
 }
 
-
 void myprintf(char* string, ...){
 	
-	int va_count;
+	int i = 0, va_count;
 	va_list ap;
 	
 	va_start(ap, string);
 	
+	while(string[i]!='\0'){
+		if (string[i]=='%'){
+			i++;
+			switch(string[i]){
+				case 'd':printint(va_arg(ap,int),string);
+				break;
+				case 's':printstring(va_arg(ap,char*));
+				break;
+				case 'c':putchar(va_arg(ap,int));
+				break;
+				case 'f':printdouble(va_arg(ap,double),string);
+				break;
+				case '%':putchar('%');
+				break;
+				
+				default: printstring("\n invalid argument type error \n");
+				
+			}
+		i++;
+		}
+		else {
+			putchar(string[i]);
+			i++;
+		}
+	}
 	va_end(ap);
 
 }
-
-
 
 void printint(int number, char* format){
 		
 		char charint[20];
 	
 		myitoa(number, charint);
-		
-		
-		
+				
 		int i=0;
 		while (charint[i]!='\0')
 				putchar(charint[i++]);
@@ -273,7 +294,7 @@ void myitoa(int number, char* answ){
 	
 }
 
-void printstirng(char* message){
+void printstring(char* message){
 	int i =0;
 	while(message[i]!='\0'){
 		putchar(message[i]);
